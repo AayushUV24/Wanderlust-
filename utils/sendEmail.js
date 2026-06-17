b9ccb473759e2c98d78b2d60a80c05a3dbed5e5f
Ayush gupta <aayush3592@gmail.com>
2026-06-17 18:02:35 +0530

    debugging signup page

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,11 +1,16 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    service: "smtp.gmail.com",
+    port:587,
+    secure:false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
+    connectionTimeout: 30000,
+    greetingTimeout: 30000,
+    socketTimeout: 30000,
 });
 
 const sendOTP = async (email, otp) => {
